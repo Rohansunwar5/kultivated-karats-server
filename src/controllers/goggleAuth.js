aab4cf990ea2257ff75974072afe711537abb649
Aaaryan-R <87854481+Aaaryan-R@users.noreply.github.com>
2025-04-16 18:46:32 +0530

    Fix

--- src/controllers/goggleAuth.js
+++ src/controllers/goggleAuth.js
@@ -1,6 +1,7 @@
 import { OAuth2Client } from 'google-auth-library';
 import { User } from "../models/users.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import bcrypt from "bcrypt";
 
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
